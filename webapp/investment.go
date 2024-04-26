06545d1e23417a5e79ef33618b6c2446b7a3ebdf
James <jwu@gozova.com>
2024-04-25 23:09:28 -0500

    api with test

---
+++ webapp/investment.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"investment/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	server := gin.Default()
+
+    server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
+	server.POST("/events", createEvent)
+
+	server.Run(":8081") // local://8080
+}
+
+
+func getEvents(context *gin.Context){
+	events := models.GetAllEvents()
+    context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context){
+   var event models.Event
+   err := context.ShouldBindJSON(&event)
+   if err != nil {
+	  context.JSON(http.StatusBadRequest, gin.H{"message": "This is wrong"})
+	  return
+   }
+   event.ID = 1
+   event.UserID = 1
+   event.Save()
+   context.JSON(http.StatusCreated, gin.H{"message":"Event created!", "event": event})
+}
+
